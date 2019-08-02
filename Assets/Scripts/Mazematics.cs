2b0acee0f83a571bde97bc74577ac2ff8bf579dc
Qkrisi <33597356+Qkrisi@users.noreply.github.com>
2019-08-02 21:19:11 +0200

    Update Mazematics.cs

--- Assets/Scripts/Mazematics.cs
+++ Assets/Scripts/Mazematics.cs
@@ -1,7 +1,7 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Webhook Test
+
 public class Mazematics : MonoBehaviour {
 
 	public Material circleMat, squareMat, triangleMat, needleMat, diamondMat, hexagonMat, starMat, heartMat, shapelessMat, testCard;
